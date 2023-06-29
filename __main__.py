7662ccf3ad1621e0194f470b4f1f43ce8d868464
tchebbi <talelkarim.chebbi@insat.ucar.tn>
2023-06-29 09:34:01 +0100

    update main.py

--- __main__.py
+++ __main__.py
@@ -1,86 +1,50 @@
-import pulumi 
+import pulumi
 import pulumi_gcp as gcp
 from pulumi_gcp.serviceaccount import IAMBinding, IAMBindingArgs, IAMBindingConditionArgs
 import os
 from google.cloud.devtools import containeranalysis_v1
+import json
 
-registry = gcp.container.Registry("my-registry",
-    location="EU",
-    project="comworkio")
 
+my_repo = gcp.artifactregistry.Repository("my-repo",
+    description="example docker repository",
+    format="DOCKER",
+    location="us-central1",
+    repository_id="my-repository")
 
 sa = gcp.serviceaccount.Account("sa",
-    account_id="my-service-account-talel2",
-    display_name="A service account for testing purpose!")
+    account_id="my-service-account",
+    display_name="A service account that has access to the artifact repository") 
 
-# Create a key for the service account
+
+
+custom_binding = gcp.artifactregistry.RepositoryIamBinding("custom-binding",
+    project=my_repo.project,
+    location=my_repo.location,
+    repository=my_repo.repository_id,
+    role="roles/artifactregistry.reader",
+    members=[f"serviceAccount:my-service-account-talel2@comworkio.iam.gserviceaccount.com"],
+)
+
 key = gcp.serviceaccount.Key("sa-key",
     service_account_id=sa.name,
     private_key_type="TYPE_GOOGLE_CREDENTIALS_FILE",
 )
 
-# Export the key value as an output
+
 pulumi.export("service_account_key", key)
+pulumi.export("repository_name", my_repo.name)
 
 
 
-
-#Get the IAM policy for the organization
-admin = gcp.organizations.get_iam_policy(bindings=[gcp.organizations.GetIAMPolicyBindingArgs(
-    role="roles/iam.serviceAccountUser",
-    members=["serviceAccount:my-service-account-talel"],
-)])
+# admin = gcp.organizations.get_iam_policy(bindings=[gcp.organizations.GetIAMPolicyBindingArgs(
+#     role="roles/artifactregistry.reader",
+#     members=[f"serviceAccount:my-service-account-talel2@comworkio.iam.gserviceaccount.com"],
+# )])
 
 
-# Define a custom role with necessary permissions and condition
-custom_role = gcp.projects.IAMCustomRole("custom-role",
-    title="Custom Role Talel",
-    description="Custom role with access to the registry and a condition",
-    permissions=[
-       "artifactregistry.repositories.uploadArtifacts"
-    ],
-    role_id="myCustomRole2",
-)
-
-
-#Modify the IAM policy for the service account using the custom role
-admin_account_iam = IAMBinding("admin-account-iam",
-    resource=sa.name,
-    args=IAMBindingArgs(
-        service_account_id=sa.name,
-        condition=IAMBindingConditionArgs(
-            expression=registry.id.apply(lambda id: f'resource.name == "{id}"'),
-            title="Match registry name"
-        ),
-        members=[sa.email.apply(lambda email: f"serviceAccount:{email}")],
-        role=custom_role.id,
-    ),
-    opts=pulumi.ResourceOptions(parent=sa)
-)
-
-pulumi.export("endpoint", registry.id)
-
-
-
-# def get_registry(registry_name, region):
-#         registry = gcp.container.get_registry(
-#             registry_id=registry_name,
-#             location=region
-#         )
-#         return registry.name
-
-
-# # Usage example
-# hashed_name = "my-registry"
-# region = "europe-west9"
-
-# registry_name = get_registry(hashed_name, region)
-# if registry_name:
-#     pulumi.export("registry_exists", True)
-#     pulumi.export("registry_name", registry_name)
-# else:
-#     pulumi.export("registry_exists", False)
-
-
-# exists = check_registry_exists(hashed_name, region)
-# pulumi.export("registry_exists", exists)
+# policy = gcp.artifactregistry.RepositoryIamPolicy("policy",
+#     project=my_repo.project,
+#     location=my_repo.location,
+#     repository=my_repo.repository_id,
+#     policy_data = admin.policy_data)
